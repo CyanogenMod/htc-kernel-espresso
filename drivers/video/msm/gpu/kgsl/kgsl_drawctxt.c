31b80890dba58cd093c2e91328fc0a7daa575acc
Shubhraprakash Das <sadas@quicinc.com>
2009-12-03 18:59:00 -0700

    msm: kgsl: backport to 2.6.29
    [ARM] msm: kgsl: switch to using per-fd page tables for GPU
    
    Instead of sharing a single, global page table, each GPU user (and fd) gets
    it's own set of page tables. This increases the per-context overhead in the
    driver, but greatly improves security and isolation.
    
    Change-Id: Ia3ad18204733cc9a4bae054f8f06cda697125640
    Cc: Shubhraprakash Das <sadas@quicinc.com>
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: properly cleanup when pagetable creation fails
    
    Change-Id: I017e36ec89e2f9435c60eae4ccaf0875824ed2b8
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: release the memory back into the free pool AFTER unmapping it
    
    Change-Id: Id91f085ef46b7a1bdbcff96b9c41c864a6943004
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: clear the dummyspace region at initialization
    
    The dummyspace area is being used to insert dummy operation for some of
    the blocks when reprogramming the page tables. Non-zero data in that
    region was causing the VGT to go off into the weeds.
    
    Change-Id: Ib124c44df2eabb303675a4a10fb36418b173259a
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: move translation error dummy space 32 bytes up
    
    Leave the bottom 32bytes of the dummy area zeroed out for other uses
    (like VGT dummy cycles). This is needed so that in case of a translation
    error, garbage written into this area does not have adverse effects.
    
    Change-Id: I3c7abe028bece82fec8c53bdf1d243dad1917746
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: Remove the need to poll for timestamp completion
    
    Use conditional RB interrupt generation when we need to wait on
    timestamps to avoid polling in userspace.
    
    Change-Id: Ib0ae4257d2fbc2b220004c4e29137ac7ae1e0589
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: fix compile error when MMU is not enabled
    
    kgsl_yamato_setstate should be static inline
    
    Change-Id: I7b4f5de07894b3fdb270d9068c18b53cafc54d14
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@quicinc.com>
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>
    
    [ARM] msm: kgsl: bug if we detect a gpu hang
    
    Change-Id: I01c5b9fdabab33cd0a4354e391c5e360e362ee29
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] msm: kgsl: Invalidate base addrs when setting page tables or flushing TLBs
    
    Change-Id: I17917aac393bec76950cbdc1978f11ee734b836b
    Signed-off-by: Shubhraprakash Das <sadas@quicinc.com>
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] video: msm: kgsl: manage the grp pclk, if present
    
    Change-Id: I950437ed8169897613651580d111c7a0c9cad638
    Signed-off-by: Dima Zavin <dima@android.com>
    
    [ARM] video: msm: kgsl: Enable shadow writes for context switching
    
    Using shadow writes for context switching gives better performance.
    
    Change-Id: Id901fba7ae7be57518ec1e800c891b0438828807
    Signed-off-by: Dima Zavin <dima@android.com>
    
    kgsl: compilation fix

--- drivers/video/msm/gpu/kgsl/kgsl_drawctxt.c
+++ drivers/video/msm/gpu/kgsl/kgsl_drawctxt.c
@@ -26,8 +26,8 @@
 #include "kgsl.h"
 #include "kgsl_log.h"
 #include "kgsl_pm4types.h"
+#include "kgsl_cmdstream.h"
 
-#define DISABLE_SHADOW_WRITES
 /*
 *
 *  Memory Map for Register, Constant & Instruction Shadow, and Command Buffers
@@ -1709,8 +1709,8 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 		if (active_ctxt->flags & CTXT_FLAGS_SHADER_SAVE) {
 			/* save shader partitioning and instructions. */
 			KGSL_CTXT_DBG("save shader");
-			kgsl_ringbuffer_issuecmds(device, 1,
-					active_ctxt->shader_save, 3);
+			kgsl_ringbuffer_issuecmds(device, KGSL_CMD_FLAGS_PMODE,
+						  active_ctxt->shader_save, 3);
 
 			/* fixup shader partitioning parameter for
 			 *  SET_SHADER_BASES.
@@ -1732,7 +1732,8 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 			for (i = 0; i < KGSL_MAX_GMEM_SHADOW_BUFFERS; i++) {
 				if (active_ctxt->user_gmem_shadow[i].gmemshadow.
 				    size > 0) {
-					kgsl_ringbuffer_issuecmds(device, 1,
+					kgsl_ringbuffer_issuecmds(device,
+						KGSL_CMD_FLAGS_PMODE,
 					  active_ctxt->user_gmem_shadow[i].
 						gmem_save, 3);
 
@@ -1744,9 +1745,10 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 				}
 			}
 			if (numbuffers == 0) {
-				kgsl_ringbuffer_issuecmds(device, 1,
-					 active_ctxt->context_gmem_shadow.
-						gmem_save, 3);
+				kgsl_ringbuffer_issuecmds(device,
+				    KGSL_CMD_FLAGS_PMODE,
+				    active_ctxt->context_gmem_shadow.gmem_save,
+				    3);
 
 				/* Restore TP0_CHICKEN */
 				kgsl_ringbuffer_issuecmds(device, 0,
@@ -1764,7 +1766,7 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 
 		KGSL_CTXT_INFO("drawctxt flags %08x\n", drawctxt->flags);
 		KGSL_CTXT_DBG("restore pagetable");
-		kgsl_mmu_setpagetable(device, drawctxt->pagetable);
+		kgsl_mmu_setstate(device, drawctxt->pagetable);
 
 		/* restore gmem.
 		 *  (note: changes shader. shader must not already be restored.)
@@ -1776,7 +1778,8 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 			for (i = 0; i < KGSL_MAX_GMEM_SHADOW_BUFFERS; i++) {
 				if (drawctxt->user_gmem_shadow[i].gmemshadow.
 				    size > 0) {
-					kgsl_ringbuffer_issuecmds(device, 1,
+					kgsl_ringbuffer_issuecmds(device,
+						KGSL_CMD_FLAGS_PMODE,
 					  drawctxt->user_gmem_shadow[i].
 						gmem_restore, 3);
 
@@ -1787,7 +1790,8 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 				}
 			}
 			if (numbuffers == 0) {
-				kgsl_ringbuffer_issuecmds(device, 1,
+				kgsl_ringbuffer_issuecmds(device,
+					KGSL_CMD_FLAGS_PMODE,
 				  drawctxt->context_gmem_shadow.gmem_restore,
 					3);
 
@@ -1813,6 +1817,7 @@ kgsl_drawctxt_switch(struct kgsl_device *device, struct kgsl_drawctxt *drawctxt,
 		cmds[1] = drawctxt->bin_base_offset;
 		kgsl_ringbuffer_issuecmds(device, 0, cmds, 2);
 
-	}
+	} else
+		kgsl_mmu_setstate(device, device->mmu.defaultpagetable);
 	KGSL_CTXT_INFO("return\n");
 }
