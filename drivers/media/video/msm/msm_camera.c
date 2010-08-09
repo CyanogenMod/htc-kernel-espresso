6200ad14ba4a5ff8afd90e15e05e4dbc3310a19e
Steve Kondik <shade@chemlab.org>
2010-08-09 01:47:48 -0400

    msm: camera: Add MSM_CAMERA_LED_DEATH_RAY
    This is a high-brightness LED mode that should never be used.
    
    Do not look into MSM_CAMERA_LED_DEATH_RAY with remaining good eye.

--- drivers/media/video/msm/msm_camera.c
+++ drivers/media/video/msm/msm_camera.c
@@ -33,6 +33,7 @@
 #include <asm/cacheflush.h>
 #include <linux/rtc.h>
 DEFINE_MUTEX(hlist_mut);
+#include <mach/msm_flashlight.h>
 
 #define MSM_MAX_CAMERA_SENSORS 5
 
@@ -1811,6 +1812,10 @@ int msm_camera_flash(struct msm_sync *sync, int level)
 	sync->sdata->flash_cfg->postpone_led_mode = MSM_CAMERA_LED_OFF;
 
 	switch (level) {
+	case MSM_CAMERA_LED_DEATH_RAY:
+		flash_level = FL_MODE_DEATH_RAY;
+		phy_flash = 1;
+		break;
 	case MSM_CAMERA_LED_LOW_FOR_SNAPSHOT:
 		/* postpone set led low*/
 		sync->sdata->flash_cfg->postpone_led_mode = MSM_CAMERA_LED_LOW;
